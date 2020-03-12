67887b812005d9e2b8d274c8f4ecc6589456b4c8
NQ-Pamp <53751334+NQ-Pamp@users.noreply.github.com>
2020-03-12 21:15:56 +0100

    Hello ASP

---
+++ ASP/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: First
+        public string Index()
+        {
+            return "Hello there";
+        }
+
+        public string Index2()
+        {
+            return "Moving on";
+        }
+
+        public string Index3(int id)
+        {
+            return "Trying params : " + id;
+        }
+
+        public string index4(int? nep)
+        {
+            return "Trying nullable param : " + nep;
+        }
+    }
+}
