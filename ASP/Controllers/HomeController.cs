87c4309cf8f498ca47a1655e61f5c246db74caf7
mtoudert <mtoudert@novaquark.com>
2020-03-27 09:33:07 +0100

    cours 2603

--- ASP/Controllers/HomeController.cs
+++ ASP/Controllers/HomeController.cs
@@ -3,18 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ASP.Models;
 
 namespace ASP.Controllers
 {
     public class HomeController : Controller
     {
         // GET: First
-        public string Index()
+        //https://localhost:44353/home/index2
+
+        public ActionResult Index(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                return View("Error");
+            }
+            else
+            {
+                //ViewData["Name"] = id;
+                ViewBag.name = id;
+                return View();
+            }
+        }
+
+        public ActionResult Index2()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Customers()
+        {
+            var customersModel = new Customers();
+            ViewBag.Customers = customersModel.GetCustomers();
+
+            return View();
+        }
+
+        public ActionResult FindCustomer(string id)
+        {
+            ViewBag.Name = id;
+
+            var customerModel = new Customers();
+            var customer = customerModel.GetCustomers().FirstOrDefault(c => c.FirstName == id);
+
+            if (customer != null)
+            {
+                ViewBag.Age = customer.Age;
+                return View("Found");
+            }
+
+            return View("NotFound");
+        }
+
+        #region MyRegion
+        public string Index_test()
         {
             return "Hello there";
         }
 
-        public string Index2()
+        public string Index_test2()
         {
             return "Moving on";
         }
@@ -28,5 +75,15 @@ namespace ASP.Controllers
         {
             return "Trying nullable param : " + nep;
         }
+
+        public string index5(string word)
+        {
+            return "Hello" + word;
+        } 
+        #endregion
+
+
+
+
     }
 }
---
+++ ASP/Models/Customers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.Models
+{
+    public class Customers
+    {
+        public List<Customer> GetCustomers()
+        {
+            return new List<Customer>()
+            {
+                new Customer(){FirstName = "Malik", LastName = "TOUDERT", Age = 30},
+                new Customer(){FirstName = "Clem", LastName = "LARCHER", Age = 29}
+            };
+        }
+    }
+}
